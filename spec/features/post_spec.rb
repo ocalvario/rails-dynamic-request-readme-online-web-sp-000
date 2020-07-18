7cec84ed1d6c24accc4cf705b2b4fae54c84c2b2
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-18 04:42:17 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -9,9 +9,12 @@ describe 'navigate' do
     visit "/posts/#{@post.id}"
     expect(page.status_code).to eq(200)
   end
+<<<<<<< HEAD
  
   it 'shows the title on the show page in an h1 tag' do
     visit "/posts/#{@post.id}"
     expect(page).to have_css("h1", text: "My Post")
   end
+=======
+>>>>>>> b0965c14dba6426e3068e2f7da6308fa60e32960
 end
